e5a959168380b2e552fcc27022e979365fc2cf8d
Edrzapi <Edrzapi@gmail.com>
2025-02-21 15:02:16 +0000

    Updating class docstrings

--- oop/Abstraction.py
+++ oop/Abstraction.py
@@ -2,24 +2,26 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):  # Vehicle is an abstract class, inheriting from ABC
+    """An Abstract Vehicle class"""
+
     def __init__(self, make, model):
         self.make = make
         self.model = model
 
     @abstractmethod
     def start_engine(self):
-        pass  # Abstract method, must be implemented by subclasses
+        """Abstract method to start the engine"""
 
     @abstractmethod
     def stop_engine(self):
-        pass  # Abstract method, must be implemented by subclasses
+        """Abstract method to stop the engine"""
 
     @abstractmethod
     def honk(self):
-        pass  # Abstract method, must be implemented by subclasses
+        """Abstract method to honk"""
 
 
-class Car(Vehicle):  # Car is a subclass of
+class Car(Vehicle):  # Car is a concrete subclass of Vehicle
     __count = 0
 
     def __init__(self, make, model, num_doors):
@@ -39,24 +41,3 @@ class Car(Vehicle):  # Car is a subclass of
 
     def honk(self):
         print(f"The {self.make} {self.model} car honks: Beep beep!")
-
-
-my_car = Car("Toyota", "t", 4)
-irfan_car = Car("Honda", "t", 4)
-harry_car = Car("Honda", "t", 4)
-print(Car.get_car_count())
-
-
-class Truck(Vehicle):  # Truck is a subclass of Vehicle
-    def __init__(self, make, model, load_capacity):
-        super().__init__(make, model)
-        self.load_capacity = load_capacity
-
-    def start_engine(self):
-        print(f"The engine of the {self.make} {self.model} truck is starting with a roar.")
-
-    def stop_engine(self):
-        print(f"The engine of the {self.make} {self.model} truck is stopping.")
-
-    def honk(self):
-        print(f"The {self.make} {self.model} truck honks loudly: HONK HONK!")
